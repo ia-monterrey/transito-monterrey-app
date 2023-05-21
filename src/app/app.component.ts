96a0ce65d32302ab2b7d4d34acd2710dd92db67d
ia mty <iartificialmonterrey@gmail.com>
2023-05-21 02:27:44 -0600

    Cambio el contenido de la App para adecuarlo al tema

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['../demo-styling.css']
 })
 export class AppComponent {
-  title = 'angular-quickstart';
+  title = 'Reglamento de Monterrey';
 }
